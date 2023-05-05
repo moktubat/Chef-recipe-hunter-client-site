1bbaf07e4f6f6a8cffc922ae83b1ddc264170da1
moktubat <shadhinreja@gmail.com>
2023-05-05 15:08:33 +0600

    data added

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Card, ListGroup } from 'react-bootstrap';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 const Recipes = ({rec}) => {
     const {id, recipe_name, recipe_image, cooking_method, ingredients, rating} = rec;
+    const [buttonDisabled, setButtonDisabled] = useState(false); // initialize buttonDisabled to false
+
     const notify = () => toast("Added to Favorite List !!");
+
+    const handleClick = () => {
+        notify(); // show toast notification
+        setButtonDisabled(true); // disable the button
+    }
 
     return (
         <div>
             <Card className='d-flex' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={recipe_image} />
-      <Card.Body>
-        <Card.Title>{recipe_name}</Card.Title>
-        <Card.Text>
-          <strong>Cooking Method : </strong>
-          {cooking_method}
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <h3 className='ps-3'>Ingredients</h3>
-      {
-        ingredients.map(ingredient => <ListGroup.Item>{ingredient}</ListGroup.Item>)
-      }
-      </ListGroup>
-      <Card.Body className='d-flex'>
-        <Card.Text>Rating: {rating}</Card.Text>
-        <div>
-        <Button onClick={notify} className='ms-4'>Add to Favorite</Button>
-        <ToastContainer />
-        </div>
-      </Card.Body>
-    </Card>
+                <Card.Img variant="top" src={recipe_image} />
+                <Card.Body>
+                    <Card.Title>{recipe_name}</Card.Title>
+                    <Card.Text>
+                        <strong>Cooking Method : </strong>
+                        {cooking_method}
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <h3 className='ps-3'>Ingredients</h3>
+                    {
+                        ingredients.map(ingredient => <ListGroup.Item>{ingredient}</ListGroup.Item>)
+                    }
+                </ListGroup>
+                <Card.Body className='d-flex'>
+                    <Card.Text>Rating: {rating}</Card.Text>
+                    <div>
+                        <Button onClick={handleClick} className='ms-4' disabled={buttonDisabled}>Add to Favorite</Button>
+                        <ToastContainer />
+                    </div>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -1,69 +1,78 @@
-import React, { useContext } from 'react';
-import { Carousel, Container, Nav, NavLink, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import Style from './Header.css';
-import Login from '../../layouts/Login';
-import { AuthContext } from '../../providers/AuthProvider';
-import slid1 from '../../assets/slide1.jpg';
-import slid2 from '../../assets/slide2.jpg';
-import slid3 from '../../assets/slide3.jpg';
-
+import React, { useContext } from "react";
+import { Carousel, Container, Nav, NavLink, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Style from "./Header.css";
+import Login from "../../layouts/Login";
+import { AuthContext } from "../../providers/AuthProvider";
+import slid1 from "../../assets/slide1.jpg";
+import slid2 from "../../assets/slide2.jpg";
+import slid3 from "../../assets/slide3.jpg";
 
 const Header = () => {
-    return (
-        <div className='header-bg'>
-                
-        <div className="container pt-5">
-        <div className="d-flex align-content-center">
-            <div className="col-md-12 col-lg-6 mt-5 pt-5 ms-e ">
-                <h1 className="display-4 fw-bold text-black">Teste your best food with a new Style!</h1>
-                <p className="text-body-secondary">Manufacturer of Pasta & Soups, Pasta & Fryums offered by The Tasty Tortellini from Rome, Italy.</p>
-                <button className="btn btn-danger">Explore Now →</button>
-            </div>
-            <div className="col-md-12 col-lg-6">
-              
+  return (
+    <div className="header-bg mb-5 pb-5">
+      <div className="container pt-5">
+        <div className="row">
+          <div className="col-12 col-md-6 my-sm-5">
+            <h1 className="display-4 fw-bold text-black">
+              Test your best food with a new style!
+            </h1>
+            <p className="text-body-secondary">
+              Manufacturer of Pasta & Soups, Pasta & Fryums offered by The Tasty
+              Tortellini from Rome, Italy.
+            </p>
+            <button className="btn btn-danger">Explore Now →</button>
+          </div>
+          <div className="col-12 col-md-6 my-sm-5">
             <Carousel>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100 img-fluid rounded opacity-75"
-          src={slid1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Pizza alla Bufala</h3>
-          <p>Never sit around and wait for someone unless they’re delivering a pizza.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100 img-fluid rounded opacity-75"
-          src={slid2}
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Giardiniera</h3>
-          <p>Giardiniera is a traditional Italian mixture of vegetables, carrots, onions, cauliflower, and cucumbers.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 img-fluid rounded opacity-75"
-          src={slid3}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Tagliata di manzo</h3>
-          <p>
-          Tagliata is a traditional Italian meat dish that's most commonly made with beef.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-            </div>
+              <Carousel.Item interval={1000}>
+                <img
+                  className="d-block w-100 img-fluid rounded opacity-75"
+                  src={slid1}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>Pizza alla Bufala</h3>
+                  <p>
+                    Never sit around and wait for someone unless they’re
+                    delivering a pizza.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item interval={500}>
+                <img
+                  className="d-block w-100 img-fluid rounded opacity-75"
+                  src={slid2}
+                  alt="Second slide"
+                />
+                <Carousel.Caption>
+                  <h3>Giardiniera</h3>
+                  <p>
+                    Giardiniera is a traditional Italian mixture of vegetables,
+                    carrots, onions, cauliflower, and cucumbers.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100 img-fluid rounded opacity-75"
+                  src={slid3}
+                  alt="Third slide"
+                />
+                <Carousel.Caption>
+                  <h3>Tagliata di manzo</h3>
+                  <p>
+                    Tagliata is a traditional Italian meat dish that's most
+                    commonly made with beef.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Header;
